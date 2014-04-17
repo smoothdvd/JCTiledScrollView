d197b95644039a2d66e2bf31941b6b910e319583
smoothdvd <madfxgao@gmail.com>
2014-04-17 10:29:42 +0800

    Display correct tile images with retina device

--- JCTiledScrollView/Source/JCTiledView.m
+++ JCTiledScrollView/Source/JCTiledView.m
@@ -1,29 +1,29 @@
 //
 //  JCTiledView.m
-//  
+//
 //  Created by Jesse Collis on 1/2/2012.
 //  Copyright (c) 2012, Jesse Collis JC Multimedia Design. <jesse@jcmultimedia.com.au>
 //  All rights reserved.
 //
-//  * Redistribution and use in source and binary forms, with or without 
+//  * Redistribution and use in source and binary forms, with or without
 //   modification, are permitted provided that the following conditions are met:
 //
-//  * Redistributions of source code must retain the above copyright 
+//  * Redistributions of source code must retain the above copyright
 //   notice, this list of conditions and the following disclaimer.
 //
-//  * Redistributions in binary form must reproduce the above copyright 
-//   notice, this list of conditions and the following disclaimer in the 
+//  * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
 //   documentation and/or other materials provided with the distribution.
 //
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY 
-//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
-//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
-//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 //  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 //
 
@@ -52,8 +52,9 @@ static const CGFloat kDefaultTileSize = 256.0f;
 {
   if (self = [super initWithFrame:frame])
   {
-    CGSize scaledTileSize = CGSizeApplyAffineTransform(self.tileSize, CGAffineTransformMakeScale(self.contentScaleFactor, self.contentScaleFactor));
-    self.tiledLayer.tileSize = scaledTileSize;
+    //CGSize scaledTileSize = CGSizeApplyAffineTransform(self.tileSize, CGAffineTransformMakeScale(self.contentScaleFactor, self.contentScaleFactor));
+    //self.tiledLayer.tileSize = scaledTileSize;
+    self.tiledLayer.tileSize = self.tileSize;
     self.tiledLayer.levelsOfDetail = 1;
     self.numberOfZoomLevels = 3;
     self.shouldAnnotateRect = NO;
@@ -80,13 +81,15 @@ static const CGFloat kDefaultTileSize = 256.0f;
 
 - (void)setNumberOfZoomLevels:(size_t)levels
 {
-  self.tiledLayer.levelsOfDetailBias = levels;
+  //self.tiledLayer.levelsOfDetailBias = levels;
+  self.tiledLayer.levelsOfDetailBias = (self.contentScaleFactor > 1.0f) ? levels - 1 : levels;
 }
 
 - (void)drawRect:(CGRect)rect
 {
   CGContextRef ctx = UIGraphicsGetCurrentContext();
-  CGFloat scale = CGContextGetCTM(ctx).a / self.tiledLayer.contentsScale;
+  //CGFloat scale = CGContextGetCTM(ctx).a / self.tiledLayer.contentsScale;
+  CGFloat scale = CGContextGetCTM(ctx).a / self.tiledLayer.contentsScale * self.contentScaleFactor;
 
   NSInteger col = (NSInteger)((CGRectGetMinX(rect) * scale) / self.tileSize.width);
   NSInteger row = (NSInteger)((CGRectGetMinY(rect) * scale) / self.tileSize.height);
